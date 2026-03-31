6a2a89cf7e32f53096179d650d8a38d0ce17fab9
xyw823171794 <823171794@qq.com>
2026-03-31 17:47:04 +0800

    first commit

---
+++ common/js/link.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+
+
+function disp1(url){
+	window.open(url, "yousei", "width=512,height=512,scrollbars=no,resizable=no,menubar=no,toolbar=no,location=no,directories=no,status=no");
+}
+function disp2(url){
+	window.open(url, "goods", "width=512,height=512,scrollbars=yes,resizable=no,menubar=no,toolbar=no,location=no,directories=no,status=no");
+}
+
+function apperPopup()
+{
+var popup=
+window.open('../../../yousei/index.html','yousei','toolbar=no,location=no,directories=no,status=no,scrollbars=no,width=512,height=512');
+popup.focus();
+}
